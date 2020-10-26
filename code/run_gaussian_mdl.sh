57372824c7d1dd77a49e59d71ab1425f4f0e7d76
anushdevv <73491251+anushdevv@users.noreply.github.com>
2020-10-26 13:26:08 -0400

    Update run_gaussian_mdl.sh

--- code/run_gaussian_mdl.sh
+++ code/run_gaussian_mdl.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-N=400
-k_mu=880
-k_sigma=300
-lambd=10
-r0=1
-s_on=10
-iterations=100
+N=400 # Number of cells on the lattice
+k_mu=880 # Mean of concentration threshold distribution 
+k_sigma=300 # Standard Deviation of concentration threshold distribution 
+lambd=10 # Signaling strength (regulates rate of decay with distance)
+r0=1 # Distance between adjacent cells on lattice
+s_on=10 # ON state concentration
+iterations=100 
 simulations=400
 
 python3 ./gaussian_mdl.py $N $k_mu $k_sigma $lambd $r0 $s_on $iterations $simulations
