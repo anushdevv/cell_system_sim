2a0cdfd02c5d67072b786dfd6fa1fa8751a8a1ce
anushdevv <73491251+anushdevv@users.noreply.github.com>
2020-11-02 20:18:40 -0500

    Update run_gaussian_mdl.sh

--- code/run_gaussian_mdl.sh
+++ code/run_gaussian_mdl.sh
@@ -22,9 +22,9 @@ show_steady=0 # 1: plot steady state for every simulation, 0: Do nothing
 
 ######### Parameter Set 2 #########
 
-N=900 # Number of cells on the lattice
+N=10000 # Number of cells on the lattice
 k_mu=5.5 # Mean of concentration threshold distribution 
-k_sigma=2 # Standard Deviation of concentration threshold distribution 
+k_sigma=1.5 # Standard Deviation of concentration threshold distribution 
 lambd=0.5 # Signaling strength (regulates rate of decay with distance)
 r0=1 # Distance between adjacent cells on lattice
 s_on=10 # ON state concentration
@@ -32,10 +32,14 @@ iterations=100
 init_state=1
 simulations=1
 show_steady=1 # 1: plot steady state for every simulation, 0: Do nothing
-num_rads=3
+num_rads=3 # Number of radii to sample
+fft_thresh=1
 
 # Analyze/visualize steady states
 #python3 ./gaussian_mdl.py $N $k_mu $k_sigma $lambd $r0 $s_on $iterations $init_state $simulations $show_steady
 
 # Evaluate function for steady states
-python3 ./gaussian_mdl_steady_eval.py $N $k_mu $k_sigma $lambd $r0 $s_on $iterations $init_state $simulations $show_steady $num_rads
+#python3 ./gaussian_mdl_steady_eval.py $N $k_mu $k_sigma $lambd $r0 $s_on $iterations $init_state $simulations $show_steady $num_rads
+
+# Evaluate function 2 for steady states
+python3 ./gaussian_mdl_steady_eval2.py $N $k_mu $k_sigma $lambd $r0 $s_on $iterations $init_state $simulations $show_steady $num_rads $fft_thresh
